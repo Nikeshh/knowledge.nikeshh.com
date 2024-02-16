608c4a8c0d9fa39e83ac2c66bbdea87a85ecc711
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-02-16 18:41:53 -0500

    Changed basic layout, modal provider, theme provider and added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: ["/"],
+  async beforeAuth(auth, req) {},
+  async afterAuth(auth, req) {}
+});
+ 
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
