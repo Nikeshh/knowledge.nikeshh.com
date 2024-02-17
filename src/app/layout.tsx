e2b8a7a7e0ea75807c8fe7a6eec43beb8b639807
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-02-16 20:49:42 -0500

    Header changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,10 @@ import { DM_Sans } from "next/font/google";
 import ModalProvider from "@/providers/modal-provider";
 import { dark } from "@clerk/themes";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navigation from "@/components/navigation";
+import Footer from "@/components/footer";
+import NotificationC from "@/components/notification";
+import { Toaster } from "@/components/ui/sonner"
 
 const font = DM_Sans({ subsets: ["latin"] });
 
@@ -29,10 +33,16 @@ export default function RootLayout({
         >
           <ModalProvider>
             <ClerkProvider appearance={{ baseTheme: dark }}>
-              {children}
+              <main className="w-full">
+                <NotificationC />
+                <Navigation />
+                {children}
+                <Footer />
+              </main>
             </ClerkProvider>
           </ModalProvider>
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
---
+++ src/components/notification/index.tsx
@@ -0,0 +1,8 @@
+const NotificationC = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default NotificationC
