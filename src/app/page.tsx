937c5b580c4eb83ae0260b8a83042f4c3fac6caa
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-02-16 22:25:21 -0500

    Added tabs and home page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,18 @@
 
 import React, { useEffect } from 'react'
 import { toast } from "sonner"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
 
 export default function Home() {
   useEffect(() => {
@@ -15,7 +27,33 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <div className='flex justify-center h-screen w-full mt-20'>
+      <Tabs defaultValue="saas" className="hidden lg:inline-block">
+        <TabsList>
+          <TabsTrigger value="saas">SAAS</TabsTrigger>
+          <TabsTrigger value="fullstackdevelopment">Full Stack Development</TabsTrigger>
+        </TabsList>
+        <TabsContent value="saas">
+          SAAS
+        </TabsContent>
+        <TabsContent value="fullstackdevelopment">
+          Full Stack Development
+        </TabsContent>
+    </Tabs>
+    <Accordion type="single" collapsible className="container w-full inline-block lg:hidden">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>SAAS</AccordionTrigger>
+          <AccordionContent>
+            SAAS
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Full Stack Development</AccordionTrigger>
+          <AccordionContent>
+            Full Stack Development
+          </AccordionContent>
+        </AccordionItem>
+    </Accordion>
     </div>
   );
 }
--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { ModeToggle } from "../global/mode-toggle";
+import { Badge } from "@/components/ui/badge"
 
 const Navigation = () => {
     return (
@@ -15,13 +16,29 @@ const Navigation = () => {
                 </ul>
             </nav>
             <aside className="flex gap-2 items-center">
+                <Badge variant="secondary">
+                    <a
+                        className="group flex justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
+                        href="#"
+                    >
+                        <span className="text-gray-700 transition group-hover:text-gray-700/75">
+                            Available to work
+                        </span>
+                        <span className="relative flex h-2 w-2">
+                            <span
+                                className="absolute inline-flex h-full w-full animate-ping rounded-full bg-teal-400 opacity-75"
+                            ></span>
+                            <span className="relative inline-flex size-2 rounded-full bg-teal-500"></span>
+                        </span>
+                    </a>
+                </Badge>
                 <Link
                     href={'/contact-me'}
                     className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
                 >
                     Contact Me
                 </Link>
-                <ModeToggle />        
+                <ModeToggle />
             </aside>
         </div>
     );
