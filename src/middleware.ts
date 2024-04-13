7804e6be31440ed05af9c0e63ca78d1a3d84e15d
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-04-12 21:27:02 -0400

    Middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/preview", "/api/revalidate"],
   async beforeAuth(auth, req) {},
   async afterAuth(auth, req) {}
 });
